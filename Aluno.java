596ebafe24bd2541e355e99dd91d0631fe46f19d
YvesPereira21 <128404191+YvesPereira21@users.noreply.github.com>
2023-08-22 19:01:08 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,25 +1,31 @@
 public class Aluno {
     private String nome;
     private String email;
-    private int matricula;
-    private int qtdCredito;
+    private Integer matricula;
+    private Integer qtdCredito;
+    
     public Aluno(String nome, String email, int matricula){
         this.nome = nome;
         this.email = email;
         this.matricula = matricula;
         qtdCredito = 0;
     }
+    
     public String getNome(){
         return nome;
     }
+    
     public String getEmail(){
         return email;
     }
+    
     public void setEmail(String novoEmail){
         email = novoEmail;
     }
     public int getMatricula(){ return matricula; }
+    
     public int getQtdCredito(){ return qtdCredito; }
+    
     public void adicionaCredito2(){
         qtdCredito += 2;
     }
